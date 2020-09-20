8f87d1255f332ea23223accd901bb6735701d53c
Anto <46758577+AntonyDalmiere@users.noreply.github.com>
2020-09-21 01:43:25 +0200

    add Kotlin support

--- NotificationScheduler/src/main/java/fr/anto/notificationscheduler/Booter.kt
+++ NotificationScheduler/src/main/java/fr/anto/notificationscheduler/Booter.kt
@@ -1,22 +1,19 @@
-package fr.anto.notificationscheduler;
+package fr.anto.notificationscheduler
 
-import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
-import java.util.List;
 /**
  * A private class used internally to start the scheduling of pending notifications.
  */
-public class Booter extends BroadcastReceiver {
+class Booter : BroadcastReceiver() {
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        List<Notification> listNotifications = NotificationDatabase.getInstance(context).notificationDAO().getBlockingAllNotifications();
-        for(Notification notif : listNotifications)
-        {
-            NotificationScheduler.scheduleNotification(context,notif.id,notif.time);
+    override fun onReceive(context: Context, intent: Intent) {
+        val listNotifications = NotificationDatabase.getInstance(context).notificationDAO().blockingAllNotifications
+        for (notif in listNotifications) {
+            NotificationScheduler.scheduleNotification(context, notif.id, notif.time)
         }
     }
 }
